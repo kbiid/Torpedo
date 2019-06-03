b69e11d695bbf44293280637a6f2e0ba48b0dfdd
torpedo <torpedo@torpedo-pc>
2019-06-03 15:48:24 +0900

    테스트 클래스

--- Study/src/BaseBallGame_TDD/GameTest.java
+++ Study/src/BaseBallGame_TDD/GameTest.java
@@ -1,7 +1,7 @@
 package BaseBallGame_TDD;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
@@ -13,30 +13,69 @@ class GameTest {
 	@Test
 	void test() {
 		Game game = new Game();
-		/*
-		 * int [] array = game.GetArray(); for(int i=0; i<array.length; i++) {
-		 * System.out.println(array[i]); }
-		 */
+		int[] array = game.getRandomNumberArray();
+
+		Assertions.assertEquals(3, array.length);
 	}
 
 	// 유저가 3개의 숫자를 입력했을 때 정보가 올바르게 저장되었는지 테스트
 	@Test
 	void test_User_EnterNumber() {
-		Game game = new Game(1, 2, 3);
+		int[] expectNumberArray = { 1, 2, 3 };
+		Game game = new Game(expectNumberArray);
 		int[] array = game.getUserArray();
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
+
+		Assert.assertArrayEquals(expectNumberArray, array);
 	}
 
-	// 스트라이크, 볼 판정 테스트
+	// 스트라이크 판정 테스트
 	@Test
-	void test_StrikeAndBall() {
-		Game game = new Game(1, 2, 3);
-		int[] array = game.getUserArray();
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
+	void test_StrikeCount() {
+		int[] problemNumberArray = { 4, 2, 3 };
+		int[] expectNumberArray = { 1, 2, 3 };
+		Game game = new Game(expectNumberArray);
+		game.setRandomNumberArray(problemNumberArray);
+		game.setStrikeCount();
+
+		Assertions.assertEquals(2, game.getStrikeCount());
+	}
+
+	// 볼 판정 테스트
+	@Test
+	void test_BallCount() {
+		int[] problemNumberArray = { 4, 2, 3 };
+		int[] expectNumberArray = { 2, 5, 1 };
+		Game game = new Game(expectNumberArray);
+		game.setRandomNumberArray(problemNumberArray);
+		game.setBallCount();
+
+		Assertions.assertEquals(1, game.getBallCount());
+	}
+
+	// 스트라이크 볼 카운트를 출력
+	@Test
+	void test_PrintCount() {
+		int[] problemNumberArray = { 4, 2, 3 };
+		int[] expectNumberArray = { 2, 5, 3 };
+		Game game = new Game(expectNumberArray);
+		game.setRandomNumberArray(problemNumberArray);
+		game.setStrikeCount();
+		game.setBallCount();
+
+		Assertions.assertEquals("1S 1B", game.printBaseballCount());
+	}
+
+	// 3S가 아닐경우 false를 리턴
+	@Test
+	void test_Is3Strike() {
+		int[] problemNumberArray = { 2, 4, 3 };
+		int[] expectNumberArray = { 2, 5, 3 };
+		Game game = new Game(expectNumberArray);
+		game.setRandomNumberArray(problemNumberArray);
+		game.setStrikeCount();
+		game.setBallCount();
+
+		Assertions.assertEquals(false, game.is3Strike());
 	}
 
 }
