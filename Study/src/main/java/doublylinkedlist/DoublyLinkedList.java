ec9ce5a203f0d8e1c506d45c5cd67c196d6dbf70
user <user@DESKTOP-6BBACVK>
2019-06-04 17:18:21 +0900

    doublyLinkedList 클래스 완료

--- Study/src/main/java/doublylinkedlist/DoublyLinkedList.java
+++ Study/src/main/java/doublylinkedlist/DoublyLinkedList.java
@@ -94,27 +94,95 @@ public class DoublyLinkedList {
 
 		return node;
 	}
-	
+
 	protected int getSize() {
 		return size;
 	}
-	
-	protected void deleteFirst() {
-		
-		if(head.getNext() == null) 
-			return;
-		
-		if(size == 1) {
+
+	protected Node deleteFirst() {
+
+		if (head.getNext() == null) {
+			System.out.println("이 doublyLinkedList에는 노드가 존재하지 않습니다.");
+			return null;
+		}
+
+		Node firstNode = getNodeByIndex(0);
+
+		if (size == 1) {
 			head.setNext(null);
 			head.setPrev(null);
-		}		
-		else {
+		} else {
 			head.setNext(head.getNext().getNext());
 			head.getNext().setPrev(head);
 		}
-		
+
 		size--;
-		
+
+		return firstNode;
+
 	}
 
+	protected Node deleteByIndex(int index) {
+
+		if (index < 0 || index >= size) {
+			System.out.println("index 오류 입니다.");
+			return null;
+		} else if (index == 0) {
+			return deleteFirst();
+		}
+
+		Node removeNode = getNodeByIndex(index);
+		Node prev = removeNode.getPrev();
+		Node next = removeNode.getNext();
+
+		prev.setNext(next);
+
+		if (next != null) {
+			next.setPrev(prev);
+		} else {
+			head.setPrev(prev);
+		}
+
+		size--;
+
+		return removeNode;
+	}
+
+	protected Node deleteLast() {
+
+		if (size == 0) {
+			System.out.println("비어있는 리스트 입니다.");
+			return null;
+		} else if (size == 1) {
+			return deleteFirst();
+		}
+
+		Node removeNode = head.getPrev();
+		Node previousNode = removeNode.getPrev();
+
+		previousNode.setNext(head);
+		head.setPrev(previousNode);
+
+		size--;
+
+		return removeNode;
+	}
+
+	protected String printDoublyLinkedList() {
+		if (head.getNext() == null && head.getPrev() == null)
+			return "[]";
+
+		Node temp = head;
+		String str = "[";
+
+		while (temp.getNext() != null) {
+			str += temp.getData() + ",";
+			temp = temp.getNext();
+		}
+
+		str += temp.getData();
+
+		return str + "]";
+
+	}
 }
