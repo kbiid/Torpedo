7ee9ecfbf31f99bf8705e9392fbb65f8c49207a1
user <user@DESKTOP-6BBACVK>
2019-06-05 15:03:59 +0900

    StackQueue에 필요한 Stack 구현 클래스들

---
+++ Study/src/main/java/queuebystack/Stack.java
@@ -0,0 +1,57 @@
+package queuebystack;
+
+public class Stack {
+	private StackNode topNode;
+	private int stackDepth;
+
+	public Stack() {
+		topNode = null;
+		stackDepth = -1;
+	}
+
+	protected void push(Object data) {
+
+		StackNode stackNode = new StackNode(data);
+
+		// 스택이 비어있는 경우
+		if (stackDepth == -1) {
+			stackNode.setNextStackNode(null);
+		}
+		// 스택이 비어있지 않은 경우
+		else {
+			stackNode.setNextStackNode(topNode);
+		}
+
+		topNode = stackNode;
+
+		stackDepth++;
+	}
+
+	protected Object pop() {
+
+		StackNode stackNode = topNode;
+
+		topNode = topNode.getNextStackNode();
+
+		stackDepth--;
+
+		return stackNode.getData();
+
+	}
+
+	protected StackNode peek() {
+
+		return topNode;
+	}
+
+	protected boolean isEmpty() {
+		if (stackDepth == -1)
+			return true;
+		else
+			return false;
+	}
+
+	protected int getStackDepth() {
+		return stackDepth;
+	}
+}
---
+++ Study/src/main/java/queuebystack/StackNode.java
@@ -0,0 +1,22 @@
+package queuebystack;
+
+public class StackNode {
+	private Object data;
+	private StackNode nextStackNode;
+
+	public StackNode(Object data) {
+		this.data = data;
+	}
+
+	protected Object getData() {
+		return data;
+	}
+
+	protected void setNextStackNode(StackNode nextStackNode) {
+		this.nextStackNode = nextStackNode;
+	}
+
+	protected StackNode getNextStackNode() {
+		return nextStackNode;
+	}
+}
