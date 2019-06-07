ce7b1b593dbfec7153776ab305400b35fad003d2
user <user@DESKTOP-6BBACVK>
2019-06-07 18:02:35 +0900

    리팩토링

--- Study/src/main/java/recursive/Recursive.java
+++ Study/src/main/java/recursive/Recursive.java
@@ -3,7 +3,11 @@ package recursive;
 //재귀 함수가 들어있는 클래스
 public class Recursive {
 
-	// 입력된 수부터 1까지 더하는 메소드
+	/**
+	 * 입력된 수부터 1까지 더하는 메소드
+	 * @param num
+	 * @return int
+	 */
 	public int recursiveMethod(int num) {
 		if (num <= 1) {
 			return 1;
